690338b1ed25782310bc46bb724a6e691437144b
Paul <paul.sg.cross@gmail.com>
2021-05-21 19:00:17 +0100

    Adds basic dual operators

--- src/Main.hx
+++ src/Main.hx
@@ -1,4 +1,5 @@
 
+import duals.DualNumber;
 import haxe.macro.Expr;
 import haxe.macro.Context;
 
@@ -10,7 +11,11 @@ class Main {
     trace(out1);
     trace(out2);
 
+    var dual1 = new DualNumber(3.0, 1.0);
+    var dual2 = new DualNumber(3.0, 1.0);
+    var dual3 = new DualNumber(3.0, 1.0);
     
+    trace(dual1 + dual2 + dual3);
   }
 
 }
--- src/duals/DualNumber.hx
+++ src/duals/DualNumber.hx
@@ -7,14 +7,30 @@ abstract DualNumber(Vector<Float>) {
     public var v(get, never) : Float;
     public var d(get, never) : Float;
 
-    public function new(v : Float, d : Float) {
+    public inline function new(v : Float, d : Float) {
         this = new Vector(2);
         this[0] = v;
         this[1] = d;
     }
 
+    @:op(A*B)
     public inline function multiply(dual : DualNumber) : DualNumber {
-        return new DualNumber(v * dual.v, (v * dual.v));
+        return new DualNumber(v * dual.v, (v * dual.d) + (d * dual.v));
+    }
+
+    @:op(A/B)
+    public inline function divide(dual : DualNumber) : DualNumber {
+        return new DualNumber(v / dual.v, ((d * dual.v) - (v * dual.d)) / (dual.v * dual.v));
+    }
+
+    @:op(A-B)
+    public inline function sub(dual : DualNumber) : DualNumber {
+        return new DualNumber(v - dual.v, d - dual.d);
+    }
+
+    @:op(A+B)
+    public inline function add(dual : DualNumber) : DualNumber {
+        return new DualNumber(v + dual.v, d + dual.d);
     }
 
     private function get_v() : Float {
@@ -23,5 +39,9 @@ abstract DualNumber(Vector<Float>) {
 
     private function get_d() : Float {
         return this[1];
+    }
+
+    public function toString() : String {
+        return v + " + " + d + "e";
     }
 }
