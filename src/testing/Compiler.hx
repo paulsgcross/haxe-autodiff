2d1d6069d2654b898de2b59db337885a974acf20
Paul <paul.sg.cross@gmail.com>
2021-05-24 15:56:24 +0100

    Updates tests

--- src/testing/Compiler.hx
+++ src/testing/Compiler.hx
@@ -2,15 +2,16 @@ package testing;
 
 class Compiler {
     static function main() {
-        
+        Test.funcMult(3.0, 3.0);
+        Test.funcMultDiff(3.0, 1.0, 3.0, 0.0);
     }
 }
 
-@:build(haxe.ad.compiler.AD.buildForward())
+@:build(haxe.ad.compiler.AutoDiff.buildForward())
 class Test {
   
-  @:diff public static function funcMult(x : Float) : Float {
-    return Math.cos(3*x);
+  @:diff public static function funcMult(x : Float, y : Float) : Float {
+    return x*x;
   }
 
   public static function test(x : Float) : Float {
