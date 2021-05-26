2cbce7315b5e7330f0d536b128efe6a0c32f0019
Paul <paul.sg.cross@gmail.com>
2021-05-26 08:55:32 +0100

    Updates test

--- src/testing/reverse/Variables.hx
+++ src/testing/reverse/Variables.hx
@@ -1,15 +1,9 @@
 package testing.reverse;
 
-import haxe.ad.duals.reverse.VariableMath;
-import haxe.ad.duals.reverse.Gradient;
-import haxe.ad.duals.reverse.Variable;
 import haxe.ad.duals.reverse.WengertList;
-import haxe.ds.Vector;
-import haxe.ad.duals.*;
 
 using haxe.ad.duals.reverse.Gradient;
 
-// TODO: Optimise this implementation.
 
 class Variables {
   static function main() {
@@ -21,10 +15,12 @@ class Variables {
 
     var x = list.createVariable(2.0);
     var y = list.createVariable(0.5);
-    var z = x*x + 3.0*x - 3.0;
+    var z = x*y + 3.0*x - 3.0*y;
 
     var gz = z.calculateGrad();
 
+    trace(z.value);
     trace(gz.wrt(x));
+    trace(gz.wrt(y));
   }
 }
